c762663d52fba78abf86cbc1ce1469165add53ae
Svyatoslav <razmyslov@viva64.com>
2019-10-21 11:55:41 +0300

    Disabled C4310 cl warning.

--- ThirdParty/compact_enc_det/util/basictypes.h
+++ ThirdParty/compact_enc_det/util/basictypes.h
@@ -74,6 +74,8 @@ typedef unsigned long long uint64;
 // and it should always be the signed version of whatever int32 is.)
 typedef signed int         char32;
 
+#pragma warning( push )
+#pragma warning( disable : 4310 )
 const uint8  kuint8max  = (( uint8) 0xFF);
 const uint16 kuint16max = ((uint16) 0xFFFF);
 const uint32 kuint32max = ((uint32) 0xFFFFFFFF);
@@ -86,6 +88,7 @@ const  int32 kint32min  = (( int32) 0x80000000);
 const  int32 kint32max  = (( int32) 0x7FFFFFFF);
 const  int64 kint64min  = (( int64) GG_LONGLONG(0x8000000000000000));
 const  int64 kint64max  = (( int64) GG_LONGLONG(0x7FFFFFFFFFFFFFFF));
+#pragma warning( pop )
 
 // A macro to disallow the copy constructor and operator= functions
 // This should be used in the private: declarations for a class
