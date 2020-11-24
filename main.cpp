1e2214e695f3610bf10f11c2d901b24556b01bae
Phillip Khandeliants <khandeliants@viva64.com>
2020-11-24 11:29:42 +0300

    feature: added stacktrace output on crashes

--- main.cpp
+++ main.cpp
@@ -5,9 +5,29 @@
 #include "application.h"
 #include "logparserworker.h"
 #include "messageparser.h"
+#include "../ThirdParty/backward.hpp"
 
 int main(int argc, const char** argv)
 {
+  backward::SignalHandling handler {
+    [](backward::Printer &printer, const backward::StackTrace &st)
+    {
+      const std::string path = "plog-converter.stacktrace.txt";
+      if (std::ofstream stream { path })
+      {
+        std::cerr << "Application has crashed. Please send file "
+                  << path 
+                  << " to support@viva64.com"
+                  << std::endl;
+        printer.print(st, stream);
+      }
+      else
+      {
+        printer.print(st, std::cout);
+      }
+    }
+  };
+
   PlogConverter::Application app;
   app.AddWorker(std::make_unique<PlogConverter::LogParserWorker>());
   return app.Exec(argc, argv);
