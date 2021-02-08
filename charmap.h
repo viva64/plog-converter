73cab9e3e76092eca926e86f3acfe915954a997d
Maxim Stefanov <stefanov@viva64.com>
2021-02-08 15:51:25 +0300

    Copyright update.

--- charmap.h
+++ charmap.h
@@ -1,6 +1,6 @@
 //  2006-2008 (c) Viva64.com Team
 //  2008-2020 (c) OOO "Program Verification Systems"
-//  2020 (c) PVS-Studio LLC
+//  2020-2021 (c) PVS-Studio LLC
 
 #ifndef CHAR_MAP
 #define CHAR_MAP
