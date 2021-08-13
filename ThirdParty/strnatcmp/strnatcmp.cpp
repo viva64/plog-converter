bdaa9cd2474d053f3abd0d8fc015f1786daf5327
GovorovViva64 <govorov@viva64.com>
2021-08-13 09:36:40 +0300

    fix Clang build and indentation

--- ThirdParty/strnatcmp/strnatcmp.cpp
+++ ThirdParty/strnatcmp/strnatcmp.cpp
@@ -43,21 +43,21 @@
 static inline int
 nat_isdigit(nat_char a)
 {
-     return isdigit((unsigned char) a);
+  return isdigit(static_cast<unsigned char>(a));
 }
 
 
 static inline int
 nat_isspace(nat_char a)
 {
-     return isspace((unsigned char) a);
+  return isspace(static_cast<unsigned char>(a));
 }
 
 
 static inline nat_char
 nat_toupper(nat_char a)
 {
-     return (nat_char)toupper((unsigned char) a);
+  return static_cast<nat_char>(toupper(static_cast<unsigned char>(a)));
 }
 
 
@@ -66,113 +66,112 @@ nat_toupper(nat_char a)
 static int
 compare_right(nat_char const *a, nat_char const *b)
 {
-     int bias = 0;
-     
-     /* The longest run of digits wins.  That aside, the greatest
-	value wins, but we can't know that it will until we've scanned
-	both numbers to know that they have the same magnitude, so we
-	remember it in BIAS. */
-     for (;; a++, b++) {
-	  if (!nat_isdigit(*a)  &&  !nat_isdigit(*b))
-	       return bias;
-	  else if (!nat_isdigit(*a))
-	       return -1;
-	  else if (!nat_isdigit(*b))
-	       return +1;
-	  else if (*a < *b) {
-	       if (!bias)
-		    bias = -1;
-	  } else if (*a > *b) {
-	       if (!bias)
-		    bias = +1;
-	  } else if (!*a  &&  !*b)
-	       return bias;
-     }
-     return 0;
+  int bias = 0;
+
+  /* The longest run of digits wins.  That aside, the greatest
+     value wins, but we can't know that it will until we've scanned
+     both numbers to know that they have the same magnitude, so we
+     remember it in BIAS. */
+  for (;; a++, b++) {
+    if (!nat_isdigit(*a) && !nat_isdigit(*b))
+      return bias;
+    else if (!nat_isdigit(*a))
+      return -1;
+    else if (!nat_isdigit(*b))
+      return +1;
+    else if (*a < *b) {
+      if (!bias)
+        bias = -1;
+    } else if (*a > *b) {
+      if (!bias)
+        bias = +1;
+    } else if (!*a && !*b)
+      return bias;
+  }
+  return 0;
 }
 #pragma warning( pop )
-
 
 #pragma warning( push )
 #pragma warning( disable : 4702)
 static int
 compare_left(nat_char const *a, nat_char const *b)
 {
-     /* Compare two left-aligned numbers: the first to have a
-        different value wins. */
-     for (;; a++, b++) {
-	  if (!nat_isdigit(*a)  &&  !nat_isdigit(*b))
-	       return 0;
-	  else if (!nat_isdigit(*a))
-	       return -1;
-	  else if (!nat_isdigit(*b))
-	       return +1;
-	  else if (*a < *b)
-	       return -1;
-	  else if (*a > *b)
-	       return +1;
-     }
-     return 0;
+  /* Compare two left-aligned numbers: the first to have a
+     different value wins. */
+  for (;; a++, b++) {
+    if (!nat_isdigit(*a)  &&  !nat_isdigit(*b))
+      return 0;
+    else if (!nat_isdigit(*a))
+      return -1;
+    else if (!nat_isdigit(*b))
+      return +1;
+    else if (*a < *b)
+      return -1;
+    else if (*a > *b)
+      return +1;
+  }
+  return 0;
 }
 #pragma warning( pop )
 
 
 static int strnatcmp0(nat_char const *a, nat_char const *b, int fold_case)
 {
-     int ai, bi;
-     nat_char ca, cb;
-     int fractional, result;
-     
-     assert(a && b);
-     ai = bi = 0;
-     while (1) {
-	  ca = a[ai]; cb = b[bi];
+  int ai, bi;
+  nat_char ca, cb;
+  int fractional, result;
 
-	  /* skip over leading spaces or zeros */
-	  while (nat_isspace(ca))
-	       ca = a[++ai];
+  assert(a && b);
+  ai = bi = 0;
+  while (1) {
+    ca = a[ai]; cb = b[bi];
 
-	  while (nat_isspace(cb))
-	       cb = b[++bi];
+    /* skip over leading spaces or zeros */
+    while (nat_isspace(ca))
+      ca = a[++ai];
 
-	  /* process run of digits */
-	  if (nat_isdigit(ca)  &&  nat_isdigit(cb)) {
-	       fractional = (ca == '0' || cb == '0');
+    while (nat_isspace(cb))
+      cb = b[++bi];
 
-	       if (fractional) {
-		    if ((result = compare_left(a+ai, b+bi)) != 0)
-			 return result;
-	       } else {
-		    if ((result = compare_right(a+ai, b+bi)) != 0)
-			 return result;
-	       }
-	  }
+    /* process run of digits */
+    if (nat_isdigit(ca)  &&  nat_isdigit(cb)) {
+      fractional = (ca == '0' || cb == '0');
 
-	  if (!ca && !cb) {
-	       /* The strings compare the same.  Perhaps the caller
-                  will want to call strcmp to break the tie. */
-	       return 0;
-	  }
+      if (fractional) {
+        if ((result = compare_left(a + ai, b + bi)) != 0)
+          return result;
+      } else {
+        if ((result = compare_right(a + ai, b + bi)) != 0)
+           return result;
+      }
+    }
 
-	  if (fold_case) {
-	       ca = nat_toupper(ca);
-	       cb = nat_toupper(cb);
-	  }
-	  
-	  if (ca < cb)
-	       return -1;
-	  else if (ca > cb)
-	       return +1;
+    if (!ca && !cb) {
+      /* The strings compare the same. Perhaps the caller
+         will want to call strcmp to break the tie. */
+      return 0;
+    }
 
-	  ++ai; ++bi;
-     }
+    if (fold_case) {
+      ca = nat_toupper(ca);
+      cb = nat_toupper(cb);
+    }
+
+    if (ca < cb)
+      return -1;
+    else if (ca > cb)
+      return +1;
+
+    ++ai; ++bi;
+  }
 }
 
 int strnatcmp(nat_char const *a, nat_char const *b) {
-     return strnatcmp0(a, b, 0);
+  return strnatcmp0(a, b, 0);
 }
 
 /* Compare, recognizing numeric string and ignoring case. */
 int strnatcasecmp(nat_char const *a, nat_char const *b) {
-     return strnatcmp0(a, b, 1);
+  return strnatcmp0(a, b, 1);
 }
