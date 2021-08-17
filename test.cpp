6fdf0b629d43bd5e9a7beb55fbe18eb3cd3276d8
GovorovViva64 <govorov@viva64.com>
2021-08-17 16:06:42 +0300

    replace Catch with Google tests

--- test.cpp
+++ test.cpp
@@ -2,77 +2,75 @@
 // 2008-2020 (c) OOO "Program Verification Systems"
 // 2020-2021 (c) PVS-Studio LLC
 
-#define CATCH_CONFIG_MAIN
-
-#include <catch.hpp>
+#include <gtest/gtest.h>
 #include "warning.h"
 #include "application.h"
 
 using namespace PlogConverter;
 
-TEST_CASE("Warning::GetErrorCode")
+TEST(plog_converter_test, WarningGetErrorCode)
 {
   Warning msg;
 
   msg.code = "V112";
-  REQUIRE(msg.GetErrorCode() == 112);
+  ASSERT_TRUE(msg.GetErrorCode() == 112);
 
   msg.code = "V001";
-  REQUIRE(msg.GetErrorCode() == 1);
+  ASSERT_TRUE(msg.GetErrorCode() == 1);
 }
 
-TEST_CASE("Warning::GetType")
+TEST(plog_converter_test, WarningGetType)
 {
   Warning msg;
 
   msg.code = "V112";
-  REQUIRE(msg.GetType() == AnalyzerType::Viva64);
+  ASSERT_TRUE(msg.GetType() == AnalyzerType::Viva64);
 
   msg.code = "V001";
-  REQUIRE(msg.GetType() == AnalyzerType::Fail);
+  ASSERT_TRUE(msg.GetType() == AnalyzerType::Fail);
 }
 
-TEST_CASE("Warning::GetVivaUrl")
+TEST(plog_converter_test, WarningGetVivaUrl)
 {
   Warning msg;
 
   msg.code = "V001";
-  REQUIRE(msg.GetVivaUrl() == "https://pvs-studio.com/en/docs/warnings/v001/");
+  ASSERT_TRUE(msg.GetVivaUrl() == "https://pvs-studio.com/en/docs/warnings/v001/");
 
   msg.code = "V101";
-  REQUIRE(msg.GetVivaUrl() == "https://pvs-studio.com/en/docs/warnings/v101/");
+  ASSERT_TRUE(msg.GetVivaUrl() == "https://pvs-studio.com/en/docs/warnings/v101/");
 
   msg.code = "V1001";
-  REQUIRE(msg.GetVivaUrl() == "https://pvs-studio.com/en/docs/warnings/v1001/");
+  ASSERT_TRUE(msg.GetVivaUrl() == "https://pvs-studio.com/en/docs/warnings/v1001/");
 
   msg.code = "Renew";
-  REQUIRE(msg.GetVivaUrl() == "https://pvs-studio.com/en/renewal/");
+  ASSERT_TRUE(msg.GetVivaUrl() == "https://pvs-studio.com/en/renewal/");
 }
 
-TEST_CASE("plog-converter -a")
+TEST(plog_converter_test, plog_converter_a)
 {
   std::vector<Analyzer> analyzers;
 
   ParseEnabledAnalyzers("all", analyzers);
-  REQUIRE(analyzers.empty());
+  ASSERT_TRUE(analyzers.empty());
 
   ParseEnabledAnalyzers("ALL", analyzers);
-  REQUIRE(analyzers.empty());
+  ASSERT_TRUE(analyzers.empty());
 
   ParseEnabledAnalyzers("GA:1,2;64:1;OP:1,2,3;CS:1;MISRA:1,2;AUTOSAR:1,2;OWASP:1,2", analyzers);
-  REQUIRE(analyzers.size() == 7);
-  REQUIRE(analyzers[0].type == AnalyzerType::General);
-  REQUIRE(analyzers[0].levels == (std::vector<int>{1, 2}));
-  REQUIRE(analyzers[1].type == AnalyzerType::Viva64);
-  REQUIRE(analyzers[1].levels == (std::vector<int>{1}));
-  REQUIRE(analyzers[2].type == AnalyzerType::Optimization);
-  REQUIRE(analyzers[2].levels == (std::vector<int>{1, 2, 3}));
-  REQUIRE(analyzers[3].type == AnalyzerType::CustomerSpecific);
-  REQUIRE(analyzers[3].levels == (std::vector<int>{1}));
-  REQUIRE(analyzers[4].type == AnalyzerType::Misra);
-  REQUIRE(analyzers[4].levels == (std::vector<int>{1, 2}));
-  REQUIRE(analyzers[5].type == AnalyzerType::Autosar);
-  REQUIRE(analyzers[5].levels == (std::vector<int>{1, 2}));
-  REQUIRE(analyzers[6].type == AnalyzerType::Owasp);
-  REQUIRE(analyzers[6].levels == (std::vector<int>{1, 2}));
+  ASSERT_TRUE(analyzers.size() == 7);
+  ASSERT_TRUE(analyzers[0].type == AnalyzerType::General);
+  ASSERT_TRUE(analyzers[0].levels == (std::vector<int>{1, 2}));
+  ASSERT_TRUE(analyzers[1].type == AnalyzerType::Viva64);
+  ASSERT_TRUE(analyzers[1].levels == (std::vector<int>{1}));
+  ASSERT_TRUE(analyzers[2].type == AnalyzerType::Optimization);
+  ASSERT_TRUE(analyzers[2].levels == (std::vector<int>{1, 2, 3}));
+  ASSERT_TRUE(analyzers[3].type == AnalyzerType::CustomerSpecific);
+  ASSERT_TRUE(analyzers[3].levels == (std::vector<int>{1}));
+  ASSERT_TRUE(analyzers[4].type == AnalyzerType::Misra);
+  ASSERT_TRUE(analyzers[4].levels == (std::vector<int>{1, 2}));
+  ASSERT_TRUE(analyzers[5].type == AnalyzerType::Autosar);
+  ASSERT_TRUE(analyzers[5].levels == (std::vector<int>{1, 2}));
+  ASSERT_TRUE(analyzers[6].type == AnalyzerType::Owasp);
+  ASSERT_TRUE(analyzers[6].levels == (std::vector<int>{1, 2}));
 }
