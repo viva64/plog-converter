90f22163420b7e8f1c374465e3890ce8344e6ff9
Олег <minatych@gmail.com>
2021-10-25 09:34:15 +0300

    Add MISRA 5.7 rule support to misracompliance

--- Formats/misracomplianceoutput.cpp
+++ Formats/misracomplianceoutput.cpp
@@ -611,7 +611,7 @@ MisraComplianceOutput::CategoriesMap &MisraComplianceOutput::Categories()
     { "Rule 5.4", { Category::Required, Compliance::NotSupported } },
     { "Rule 5.5", { Category::Required, Compliance::NotSupported } },
     { "Rule 5.6", Category::Required },
-    { "Rule 5.7", { Category::Required, Compliance::NotSupported } },
+    { "Rule 5.7", Category::Required },
     { "Rule 5.8", { Category::Required, Compliance::NotSupported } },
     { "Rule 5.9", { Category::Advisory, Compliance::NotSupported } },
 
