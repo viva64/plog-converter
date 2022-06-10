d88bfb05d06538fb0322a2094257817576e6986d
Oleg Lisiy <lisiy@viva64.com>
2022-06-10 11:45:04 +0300

    Add copyright to new files

--- argsextentions.cpp
+++ argsextentions.cpp
@@ -1,3 +1,7 @@
+//  2006-2008 (c) Viva64.com Team
+//  2008-2020 (c) OOO "Program Verification Systems"
+//  2020-2022 (c) PVS-Studio LLC
+
 #include "argsextentions.h"
 
 StringSplitter::StringSplitter(std::vector<std::string> separators)
